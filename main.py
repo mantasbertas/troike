0b7f731845022ddfdc4a12999720314af608e8e1
Mantas Bertasius <33432592+mantasbertas@users.noreply.github.com>
2024-07-28 15:21:56 +0300

    Add functionality to play multiple cards at once, special 9 behaviour, check for winner, create bot class that plays random cards. Still to fix: after playing 5 as the last hand card and picking up face up cards, no extra round is given

---
+++ main.py
@@ -0,0 +1,31 @@
+from game_class import Game
+
+
+# deck = Deck()
+# print("Deck before shuffling:")
+# print(deck.cards)
+#
+# deck.shuffle()
+# print("\nDeck after shuffling:")
+# print(deck.cards)
+#
+# card_drawn = deck.draw_cards()
+# print(f"\nCard drawn: {card_drawn}")
+# print(f"\nRemaining cards in the deck: {len(deck)}")
+# print("\nDeck after drawing:")
+# print(deck.cards)
+
+
+def play_game():
+    # player_names = input("Enter player names, separated by space: ").split()
+    player_names = ['Mantas', 'Jonas', 'Martynas']
+    game = Game(*player_names)
+    game.start_game()
+
+    while not game.is_game_over():
+        game.turn()
+
+    print("Game over!")
+
+
+play_game()
